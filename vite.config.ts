1e0e272793e602c777ed18a1c68c3a96a5c22fe0
svetneyro83-dev <svetneyro83@gmail.com>
2026-05-13 22:33:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // Самая важная строка, чтобы пути к стилям стали правильными:
-  base: "./pill-pal-pro-54/", 
-  plugins: [react()],
+  base: "./", // Точка и слэш заставят сайт искать файлы в текущей папке
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
